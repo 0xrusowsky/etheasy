0f5e426b11b579819538742ff0c4197228bc7d57
0xrusowsky <0xrusowsky@proton.me>
2024-04-28 01:24:23 +0200

    style: block focus and blur

--- src/components/block.rs
+++ src/components/block.rs
@@ -1,19 +1,14 @@
-use std::cmp::max;
-
 use super::super::{
     app::ScreenSize,
-    parser::{
-        self,
-        types::ParseResult,
-        utils::{count_chars, format_size},
-    },
+    parser::{self, utils},
 };
 use gloo_console::log;
-use web_sys::{Element, HtmlTextAreaElement};
+use web_sys::HtmlTextAreaElement;
 use yew::prelude::*;
 use yew::Component;
 
 pub enum Msg {
+    Blur,
     InputChanged(TextAreaInput),
     CheckForEnter(KeyboardEvent),
 }
@@ -25,6 +20,8 @@ pub struct BlockProps {
     // app state
     pub toggle: bool,
     pub size: ScreenSize,
+    pub block_count: usize,
+    pub block_id: usize,
 }
 
 #[derive(Debug, PartialEq)]
@@ -35,7 +32,7 @@ pub struct TextAreaInput {
 
 #[derive(Debug)]
 pub struct BlockComponent {
-    // block state
+    initialized: bool,
     input: TextAreaInput,
     min_height: i32,
     dec: String,
@@ -45,6 +42,7 @@ pub struct BlockComponent {
 impl BlockComponent {
     pub fn new() -> Self {
         Self {
+            initialized: false,
             input: TextAreaInput {
                 value: "".to_string(),
                 height: 0,
@@ -62,37 +60,13 @@ impl BlockComponent {
 
         for s in split {
             let p = parser::parse(s);
-            let (dec, hex) = parser::utils::stringify(p, full, size);
+            let (dec, hex) = utils::stringify(p, full, size);
             output_dec = format!("{}{}\n", output_dec, dec);
             output_hex = format!("{}{}\n", output_hex, hex);
         }
 
-        let output_lb: i32 = max(
-            count_chars(&output_dec, "\n"),
-            count_chars(&output_hex, "\n"),
-        )
-        .try_into()
-        .unwrap();
-
         if self.input.height > 136 {
-            match size {
-                ScreenSize::XS => {
-                    let lb = self.input.height / 16;
-                    self.min_height = max(110, lb * 16);
-                }
-                _ => {
-                    // log!(
-                    //     "self.input.height",
-                    //     self.input.height,
-                    //     "min_height",
-                    //     self.input.height - 60
-                    // );
-                    // let adj = (self.input.height - 136) / 20 - 1;
-                    // log!("adj", adj, "min_height", self.input.height - adj * 20);
-                    // self.min_height = self.input.height - adj;
-                    self.min_height = self.input.height;
-                }
-            }
+            self.min_height = self.input.height;
         }
         self.dec = output_dec;
         self.hex = output_hex;
@@ -110,6 +84,7 @@ impl Component for BlockComponent {
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::InputChanged(input) => {
+                self.initialized = true;
                 self.input = input;
                 self.parse_input(ctx.props().toggle, ctx.props().size);
                 // Manually resize textarea to avoid scrollbars
@@ -118,34 +93,44 @@ impl Component for BlockComponent {
                         Ok(_) => (),
                         Err(_) => log!("Failed to remove style attribute"),
                     }
-                    log!(
-                        "scroll_height",
-                        textarea.scroll_height(),
-                        "client_height",
-                        textarea.client_height()
-                    );
                     if textarea.scroll_height() > textarea.client_height() {
                         textarea
                             .set_attribute("style", &format!("height: {}px", self.min_height))
                             .expect("Failed to set style");
                     }
-                    log!("textarea.style", textarea.get_attribute("style"));
                 }
                 true
             }
             Msg::CheckForEnter(e) => {
                 let has_content = self.input.value.len() > 0
-                    && self.input.value.len() != count_chars(&self.input.value, "\n");
+                    && self.input.value.len() != utils::count_chars(&self.input.value, "\n");
                 if e.key() == "Enter" && !e.shift_key() && has_content {
                     e.prevent_default();
                     ctx.props().on_enter.emit(());
                 }
                 false
             }
+            Msg::Blur => {
+                if let Some(textarea) = ctx.props().textarea_ref.cast::<HtmlTextAreaElement>() {
+                    // Ensure first block is always expanded despite not focused
+                    if self.input.value == "" && ctx.props().block_id == ctx.props().block_count {
+                        match textarea.remove_attribute("style") {
+                            Ok(_) => (),
+                            Err(_) => log!("Failed to remove style attribute"),
+                        }
+                        textarea
+                            .set_attribute("style", "height: 110px")
+                            .expect("Failed to set style");
+                    }
+                }
+                true
+            }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_key_down = ctx.link().callback(Msg::CheckForEnter);
+        let on_blur = ctx.link().callback(move |_: FocusEvent| Msg::Blur);
         let on_text_input = ctx.link().callback(move |e: InputEvent| {
             let input: HtmlTextAreaElement = e.target_unchecked_into::<HtmlTextAreaElement>();
             Msg::InputChanged(TextAreaInput {
@@ -153,7 +138,7 @@ impl Component for BlockComponent {
                 height: input.scroll_height(),
             })
         });
-        let on_key_down = ctx.link().callback(Msg::CheckForEnter);
+
         html! {
             <div class="grid h-full grid-cols-3 p-4 pb-0 border-b-2 border-gray-100/25 dark:border-b-4 dark:border-dark-primary">
                 <div class="peer/input col-span-1 pt-0 pr-2">
@@ -161,9 +146,17 @@ impl Component for BlockComponent {
                     <textarea ref={ctx.props().textarea_ref.clone()}
                         oninput={on_text_input}
                         onkeydown={on_key_down}
+                        onblur={on_blur}
                         class="w-full h-full focus:min-h-[110px] font-mono focus-within:text-gray-50 placeholder-gray-600 bg-transparent border-0 appearance-none resize-none focus:outline-none focus:ring-0 focus:border-0 active:border-0"
                         data-gramm="false"
-                        placeholder="\n1 ether to gwei\nnow - unix(2023,12,31)\naddress(0)\n0x1234 + 5678">
+                        placeholder={
+                            // Only first block should have a placeholder
+                            if ctx.props().block_id == ctx.props().block_count {
+                                "1 ether to gwei\nnow - unix(2023,12,31)\naddress(0)\n0x1234 + 5678"
+                            } else {
+                                ""
+                            }
+                        }>
                     </textarea>
                 </div>
             if ctx.props().toggle {
